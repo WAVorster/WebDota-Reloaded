31c0bf5268c01acabe430ae675084a0c03852ce4
Wade Vorster <wade@pricecheck.co.za>
2014-12-30 15:18:04 +0200

    Initial database setup

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'default-laravel',
+			'database'  => 'webdota',
 			'username'  => 'root',
 			'password'  => 'password',
 			'charset'   => 'utf8',
--- app/database/migrations/2014_12_30_093644_CreateUsersTable.php
+++ app/database/migrations/2014_12_30_093644_CreateUsersTable.php
@@ -15,11 +15,76 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('first_name');
-			$table->string('last_name');
+			$table->string('full_name');
+			$table->string('account_name');
 			$table->string('email');
 			$table->string('password');
+			$table->char('gender',1);
+			$table->integer('age');
+			$table->string('country');
+			$table->boolean('shared_ip');
+			$table->boolean('proxy');
 			$table->string('remember_token')->nullable();
+			$table->timestamps();
+		});
+		
+		Schema::create('games', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->dateTime('started_at');
+			$table->dateTime('finished_at')->nullable();
+			$table->integer('sentinal_players');
+			$table->integer('scourge_players');
+		});
+		
+		Schema::create('clans', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('game_id');
+			$table->char('side',1);
+			$table->string('name');
+			$table->string('avatar');
+			$table->string('description');
+			$table->integer('owner_user_id');
+			$table->integer('assist_user_id')->nullable();
+			$table->integer('honour');
+			$table->integer('members');
+			$table->integer('stat_war_won');
+			$table->integer('stat_war_lost');
+			$table->timestamps();
+		});
+		
+		Schema::create('user_game_stats', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id');
+			$table->char('side',1);
+			$table->integer('hero_id');
+			$table->integer('clan_id')->nullable();
+			$table->integer('stat_strength');
+			$table->integer('stat_agility');
+			$table->integer('stat_intellegence');
+			$table->integer('stat_kills')->default(0);
+			$table->integer('stat_deaths')->default(0);
+			$table->integer('stat_consecutive_kills')->default(0);
+			$table->integer('stat_assist')->default(0);
+			$table->integer('stat_level')->default(1);
+			$table->integer('stat_exp')->default(0);
+			$table->integer('stat_creeps')->default(0);
+			$table->timestamps();
+		});
+		
+		Schema::create('heros', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('description');
+			$table->string('avatar');
+			$table->char('side',1);
+			$table->char('stat_primary',1);
+			$table->integer('stat_strength');
+			$table->integer('stat_agility');
+			$table->integer('stat_intellegence');
 			$table->timestamps();
 		});
 	}
@@ -32,6 +97,10 @@ class CreateUsersTable extends Migration {
 	public function down()
 	{
 		Schema::drop('users');
+		Schema::drop('games');
+		Schema::drop('clans');
+		Schema::drop('user_game_stats');
+		Schema::drop('heros');
 	}
 
 }
